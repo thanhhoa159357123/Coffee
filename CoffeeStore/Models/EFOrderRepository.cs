325849a5bd662acfb9f56abded9e2ab8a39c67b3
Admin <Admin@DESKTOP-9S6IOG5>
2024-07-15 22:23:20 +0700

    thêm file css

--- CoffeeStore/Models/EFOrderRepository.cs
+++ CoffeeStore/Models/EFOrderRepository.cs
@@ -21,6 +21,7 @@ namespace CoffeeStore.Models
             }
             context.SaveChanges();
         }
+
     }
 
 }
--- CoffeeStore/Models/IOrderRepository.cs
+++ CoffeeStore/Models/IOrderRepository.cs
@@ -3,6 +3,7 @@
     public interface IOrderRepository
     {
         IQueryable<Order> Orders { get; }
+
         void SaveOrder(Order order);
     }
 
